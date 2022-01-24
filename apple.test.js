a11c4f735b4be9d591e7c0d6a90f19eccc738ba6
miketolcer <miketolcer@yahoo.com>
2022-01-24 14:12:27 -0500

    changes

--- apple.test.js
+++ apple.test.js
@@ -10,27 +10,36 @@ jest.setTimeout(30000)
 //     console.log("hello everyone")
 // })
 
-test("polestar newsletter", async () => {
+test("add air pods to cart", async () => {
     await driver.get('https://www.apple.com/shop/product/MME73AM/A/airpods-3rd-generation')
 
     await driver.findElement(By.css('button[id="add-to-cart"]')).click()
 })
 
 
-test("j", async () => {
+test("support search bar", async () => {
     await driver.get('https://support.apple.com/')
 
     await driver.findElement(By.css('a[id="ac-gn-link-search"]')).click()
 
     await driver.findElement(By.css('input[id="ac-gn-searchform-input"]')).sendKeys("forgot apple id")
+    await driver.sleep(1000)
 
     await driver.findElement(By.css('button[id="ac-gn-searchform-submit"]')).click()
 
     await driver.findElement(By.css('a[id="ac-gn-link-search"]')).click()
+    await driver.sleep(3000)
 
     await driver.findElement(By.css('input[id="ac-gn-searchform-input"]')).sendKeys("broke my screen")
 
     await driver.findElement(By.css('button[id="ac-gn-searchform-submit"]')).click()
 
 })
+test("Apple store location", async () => {
+    await driver.get('https://www.apple.com/retail/')
 
+    await driver.findElement(By.css('input[aria-label="Find a store"]')).sendKeys("warren ,MI")
+})
+test("H", async () => {
+    await driver.get('https://www.apple.com/shop/product/MV7N2AM/A/airpods-with-charging-case')
+})
